b82edaa5d7ff90122834909dad8be357f2736773
CMDR-WDX <83889860+CMDR-WDX@users.noreply.github.com>
2025-12-13 21:58:10 +0100

    wip(docs): start writing out new docs, adjust astro template

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,34 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import starlightThemeFlexoki from "starlight-theme-flexoki";
+import inspectUrls from "@jsdevtools/rehype-url-inspector";
+import starlightImageZoom from "starlight-image-zoom";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://elite-dangerous-plugin-framework.github.io",
+  markdown: {
+    rehypePlugins: [
+      // @ts-ignore
+      [
+        inspectUrls,
+        {
+          selectors: ["a[href]"],
+          // @ts-ignore
+          inspectEach(url) {
+            url.node.properties.target = "_blank";
+          },
+        },
+      ],
+    ],
+  },
   integrations: [
     starlight({
       title: "Elite: Dangerous Plugin Framework",
-      plugins: [starlightThemeFlexoki({ accentColor: "orange" })],
+      plugins: [
+        starlightThemeFlexoki({ accentColor: "orange" }),
+        starlightImageZoom(),
+      ],
       social: [
         {
           icon: "github",
@@ -17,17 +37,20 @@ export default defineConfig({
           href: "https://github.com/Elite-Dangerous-Plugin-Framework/elite-dangerous-plugin-framework",
         },
       ],
+      customCss: ["./src/styles/custom.css"],
+
       sidebar: [
         {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
-          ],
+          label: "For Users",
+          autogenerate: { directory: "users" },
         },
         {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
+          label: "For Plugin Authors",
+          autogenerate: { directory: "plugin-devs" },
+        },
+        {
+          label: "Internal Docs",
+          autogenerate: { directory: "internal" },
         },
       ],
     }),
